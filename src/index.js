aaa621a4783bb04c6eb29bbe1396ee38ebe1305c
tuan <pro03tuan@gmail.com>
2025-01-08 08:41:40 +0700

    router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import New from './New';
 
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index:true,
+          element: <New />,
+        }
+      ]
+    }
+  ]
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
